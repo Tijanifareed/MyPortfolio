876135f68db6ef31404f5046364f9850071f7079
~~Agent_freddie~~🔫 <fareedtijani2810@gmail.com>
2025-02-03 13:32:12 +0100

    first commit

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,15 @@
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <h1 className="my-20 text-center text-4xl">
+          Technologies
+      </h1>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+          <div className="rounded-2xl"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Technologies
