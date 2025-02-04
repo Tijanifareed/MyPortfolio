235cffbb4186a9d8f09a1408125ad5b818b84cbc
~~Agent_freddie~~🔫 <fareedtijani2810@gmail.com>
2025-02-05 00:08:52 +0100

    cv file added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 
 
+
 const NavBar = () => {
   return(
      <nav className=" mb-20 flex items-center justify-between py-6">
@@ -12,6 +13,11 @@ const NavBar = () => {
                <h1 className="mx-2 w-10 text-4xl">FT</h1>
           </div>
           <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+          <a href="fareedcv.pdf" target="_blank" download class="px-6 py-3 bg-transparent-500 text-white text-lg font-semibold hover:bg-violet-900">
+               CV
+          </a>
+
+
                <a href="https://www.linkedin.com/in/fareed-tijani-b693492b9/"><FaLinkedin/></a>
                
                <a href="https://github.com/Tijanifareed"><FaGithub/></a>
