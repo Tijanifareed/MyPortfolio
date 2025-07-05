e5cd12e16b7e519fde099656839d09bc4d24bf90
~~Agent_freddie~~🔫 <fareedtijani2810@gmail.com>
2025-07-06 00:48:10 +0100

    removed change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
      <nav className=" mb-20 flex items-center justify-between py-6 ">
           <div className="flex flex-shrink-0 items-center">
                {/* <img className="mx-2 w-10" src={logo} alt="logo" /> */}
-               <h1 className="mx-2 w-10 text-4xl">FT</h1>
+               <h1 className="mx-2 w-10 text-4xl">FT.</h1>
           </div>
           <div className="m-8 flex items-center justify-center gap-4 text-2xl">
           {/* <a href=".../public/fareedcv.pdf" target="_blank" download className="px-6 py-3 bg-transparent-500 text-white text-lg font-semibold ">
